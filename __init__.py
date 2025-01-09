1775c07f280474ba871dd2473aed1cadbb09d765
Sandy Corsillo <the_dusky@icloud.com>
2025-01-09 12:58:05 -0500

    feat: Update __init__.py to include EmProps node
    - Import EmProps_VideoCombine node
    - Merge with base node mappings
    - Add custom display name
    
    (cherry picked from commit 6e7639d17fa04d781ed694e43f02d4bdeaa0956c)

--- __init__.py
+++ __init__.py
@@ -1,8 +1,22 @@
-from .videohelpersuite.nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .videohelpersuite.nodes import NODE_CLASS_MAPPINGS as BASE_NODE_CLASS_MAPPINGS
+from .videohelpersuite.nodes import NODE_DISPLAY_NAME_MAPPINGS as BASE_NODE_DISPLAY_NAME_MAPPINGS
+from .videohelpersuite.emprops_nodes import EmProps_VideoCombine
 import folder_paths
 from .videohelpersuite.server import server
 from .videohelpersuite import documentation
 
 WEB_DIRECTORY = "./web"
+
+# Merge base nodes with EmProps nodes
+NODE_CLASS_MAPPINGS = {
+    **BASE_NODE_CLASS_MAPPINGS,
+    "EmProps_VideoCombine": EmProps_VideoCombine
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    **BASE_NODE_DISPLAY_NAME_MAPPINGS,
+    "EmProps_VideoCombine": "EmProps Video Combine 🎥🅴🅿"
+}
+
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
 documentation.format_descriptions(NODE_CLASS_MAPPINGS)
